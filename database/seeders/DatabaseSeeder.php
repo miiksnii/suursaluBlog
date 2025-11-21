18b40bcf77d7e343edec777f5ab3cbe1637af35b
Kaspar Bergert <kasparbergert6@gmail.com>
2025-11-21 14:36:15 +0200

    feat: added posts created migrations created seeding for the posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Seed default user
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Seed posts
+        $this->call([
+            PostSeeder::class,
+        ]);
     }
+
 }
